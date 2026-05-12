f1f5308f190f1f972e419f688d0d0953591b4629
FITO0406 <fesaro0406@gmail.com>
2026-05-12 22:10:11 +0200

    Fix HTML structure in ResourceCard component

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -1,10 +1,10 @@
 export default function ResourceCard({ recurso }: any) {
             return (
                           <div className="bg-white border p-6">
-                                <h3>{recurso.titulo}</h3>h3>
-                                <p>{recurso.descripcion}</p>p>
-                                <a href={recurso.url_recurso} target="_blank">Ver recurso</a>a>
-                          </div>div>
+                                <h3>{recurso.titulo}</h3>
+                                <p>{recurso.descripcion}</p>
+                                <a href={recurso.url_recurso} target="_blank">Ver recurso</a>
+                          </div>
                         );
 }
-</div>
+<div>
