9cb2d6083859750bbccaefa69287028bb576545d
FITO0406 <fesaro0406@gmail.com>
2026-05-12 21:20:13 +0200

    Make Recurso properties optional

--- types/recurso.ts
+++ types/recurso.ts
@@ -1,20 +1,20 @@
 export interface Recurso {
-  id?: number;
-  consulta?: string;
-  identificador_oai: string;
-  titulo: string;
-  descripcion: string;
-  materia: string;
-  idioma: string;
-  fecha: string;
-  url_recurso: string;
-  fuente: string;
-  endpoint_consultado: string;
-  raw_metadata: any;
-  created_at?: string;
+    id?: number;
+    consulta?: string;
+    identificador_oai?: string;
+    titulo?: string;
+    descripcion?: string;
+    materia?: string;
+    idioma?: string;
+    fecha?: string;
+    url_recurso?: string;
+    fuente?: string;
+    endpoint_consultado?: string;
+    raw_metadata?: any;
+    created_at?: string;
 }
 
 export interface AgregaResponse {
-  recursos: Recurso[];
-  total: number;
+    recursos: Recurso[];
+    total: number;
 }
